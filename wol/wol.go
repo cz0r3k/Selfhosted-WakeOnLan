a37d8c07a71a2e45e3efbbdfbc08a9567e7b4006
Piotr Wieczorek <czorek@protonmail.com>
2022-10-29 15:36:07 +0200

    add more env and update go app

--- wol/wol.go
+++ wol/wol.go
@@ -12,41 +12,54 @@ import (
 
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
-	http.HandleFunc("/wol", getWol)
-	err := http.ListenAndServe(":8080", nil)
-	if errors.Is(err, http.ErrServerClosed) {
-		log.Fatal("server closed\n")
-	} else if err != nil {
+	web_port := os.Getenv("WEB_PORT")
+	sending_port := os.Getenv("SENDING_PORT")
+	receiving_port := os.Getenv("RECEIVING_PORT")
+	broadcast_ip := os.Getenv("BROADCAST_IP")
+	mac := os.Getenv("MAC")
+
+	magic_packet := createMagicPacket(mac)
+	local_addr, err := net.ResolveUDPAddr("udp", (":" + sending_port))
+	if err != nil {
 		log.Fatal(err)
 	}
+	remote_addr, err := net.ResolveUDPAddr("udp", (broadcast_ip + ":" + receiving_port))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	arg := wol_arg{
+		local_addr:   local_addr,
+		remote_addr:  remote_addr,
+		magic_packet: magic_packet}
+
+	http.Handle("/", fs)
+	http.HandleFunc("/wol", arg.getWol)
+	err_server := http.ListenAndServe(":"+web_port, nil)
+	if errors.Is(err_server, http.ErrServerClosed) {
+		log.Fatal("server closed\n")
+	} else if err_server != nil {
+		log.Fatal(err_server)
+	}
 }
-func getWol(w http.ResponseWriter, r *http.Request) {
-	ip := os.Getenv("IP")
-	mac := os.Getenv("MAC")
-	sendMagicPacker(mac, ip)
+
+type wol_arg struct {
+	local_addr   *net.UDPAddr
+	remote_addr  *net.UDPAddr
+	magic_packet []byte
+}
+
+func (arg wol_arg) getWol(w http.ResponseWriter, r *http.Request) {
+	sendMagicPacker(arg)
 	w.WriteHeader(200)
 }
-func sendMagicPacker(mac string, ip string) {
-	mp := createMagicPacket(mac)
-
-	laddr, err := net.ResolveUDPAddr("udp", ":8089")
+func sendMagicPacker(arg wol_arg) {
+	conn, err := net.DialUDP("udp", arg.local_addr, arg.remote_addr)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	raddr, err := net.ResolveUDPAddr("udp", ip+":9")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	conn, err := net.DialUDP("udp", laddr, raddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	defer conn.Close()
-	conn.Write(mp)
+	conn.Write(arg.magic_packet)
 }
 
 func createMagicPacket(mac string) []byte {
